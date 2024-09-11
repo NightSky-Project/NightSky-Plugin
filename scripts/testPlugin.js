bcec8b3678bec49c753a20b44c5ebb0f75afafcc
Rafael-BD <rafael8885326@gmail.com>
2024-09-11 18:11:04 -0300

    Update testPlugin.js: Change plugin to modify text color of all elements

--- scripts/testPlugin.js
+++ scripts/testPlugin.js
@@ -1,5 +1,5 @@
-// Test plugin script that highlights all paragraphs on the page
+// Test plugin script that changes the text color of all elements on the page
 (function() {
-    const paragraphs = document.querySelectorAll('p');
-    paragraphs.forEach(p => p.style.backgroundColor = 'yellow');
+    const elements = document.querySelectorAll('*');
+    elements.forEach(el => el.style.color = 'blue');
 })();
