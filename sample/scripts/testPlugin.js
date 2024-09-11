2db035850396700cb40d17fdb1b91f07863c5bb6
Rafael-BD <rafael8885326@gmail.com>
2024-09-10 22:03:33 -0300

    add sample plugin

--- sample/scripts/testPlugin.js
+++
@@ -1,5 +0,0 @@
-// Test plugin script that highlights all paragraphs on the page
-`(function() {
-    const paragraphs = document.querySelectorAll('p');
-    paragraphs.forEach(p => p.style.backgroundColor = 'yellow');
-})();`
