20e7cdbf763add0feccb30ec9c5dbf747bf11bcb
Rafael-BD <rafael8885326@gmail.com>
2024-09-12 02:05:09 -0300

    fix: add url change event observer

--- scripts/createTrendingTopic.js
+++ scripts/createTrendingTopic.js
@@ -43,27 +43,53 @@ function isRootUrl() {
     return window.location.pathname === '/search';
 }
 
-// Grants that the script will apply changes correctly after the page is fully loaded or when the DOM changes
-if (isRootUrl()) {
-    if (document.readyState === 'complete') {
-        addTrendingTopics();
-    } else {
-        document.addEventListener('DOMContentLoaded', addTrendingTopics);
-        window.addEventListener('load', addTrendingTopics);
-    }
+// Observe URL changes and reapply if necessary
+function onUrlChange(callback) {
+    let oldHref = document.location.href;
 
-    // Observe DOM changes and reapply if necessary
+    const body = document.querySelector("body");
     const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            addTrendingTopics();
-        });
+        if (oldHref !== document.location.href) {
+            oldHref = document.location.href;
+            callback();
+        }
     });
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
+    observer.observe(body, { childList: true, subtree: true });
+
+    window.addEventListener('popstate', () => {
+        callback();
     });
 }
-else {
-    console.error('Trending Topics script not executed');
+
+// Grants that the script will apply changes correctly after the page is fully loaded or when the DOM changes
+function initTrendingTopics() {
+    if (isRootUrl()) {
+        if (document.readyState === 'complete') {
+            addTrendingTopics();
+        } else {
+            document.addEventListener('DOMContentLoaded', addTrendingTopics);
+            window.addEventListener('load', addTrendingTopics);
+        }
+
+        // Observe DOM changes and reapply if necessary
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                addTrendingTopics();
+            });
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    } else {
+        console.error('Trending Topics script not executed');
+    }
 }
+
+// Initialize on page load
+initTrendingTopics();
+
+// Reinitialize on URL change
+onUrlChange(initTrendingTopics);
