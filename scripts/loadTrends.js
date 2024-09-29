b97e6cb84add92f1f83611b686ea7eb5f09ee110
Rafael-BD <rafael8885326@gmail.com>
2024-09-28 23:23:10 -0300

    feat: add dynamic loading of trending topics with language support and URL change handling

--- scripts/loadTrends.js
+++ scripts/loadTrends.js
@@ -1,12 +1,95 @@
-(async function() {
+async function getTrends() {
     const trendingTopicsDiv = document.querySelector('.trending-topics');
-    const lang = navigator.language || 'pt';
+    if (!trendingTopicsDiv) {
+        setTimeout(getTrends, 1000);
+    }
+    const lang = navigator.language.startsWith('pt') ? 'pt' : 'en';
+    console.log('lang', lang);
     const apiUrl = `https://bsky-trends.deno.dev/trend?lang=${lang}`;
 
+    const translations = {
+        categories: {
+            science: { en: 'Science', pt: 'Ciência' },
+            music: { en: 'Music', pt: 'Música' },
+            politics: { en: 'Politics', pt: 'Política' },
+            entertainment: { en: 'Entertainment', pt: 'Entretenimento' },
+            sports: { en: 'Sports', pt: 'Esportes' },
+            technology: { en: 'Technology', pt: 'Tecnologia' },
+            health: { en: 'Health', pt: 'Saúde' },
+            none: { en: '', pt: '' },
+            lgbt: { en: 'LGBT', pt: 'LGBT' },
+            economy: { en: 'Economy', pt: 'Economia' },
+            education: { en: 'Education', pt: 'Educação' },
+            environment: { en: 'Environment', pt: 'Meio Ambiente' },
+            food: { en: 'Food', pt: 'Comida' },
+            lifestyle: { en: 'Lifestyle', pt: 'Estilo de Vida' },
+            religion: { en: 'Religion', pt: 'Religião' },
+            social: { en: 'Social', pt: 'Social' },
+            travel: { en: 'Travel', pt: 'Viagem' }
+        },
+        posts: {
+            en: 'k Posts',
+            pt: 'mil Posts'
+        },
+        showMore: {
+            en: 'Show more',
+            pt: 'Mostrar mais'
+        },
+        trendingTopics: {
+            en: 'Trending Topics',
+            pt: 'Tópicos em Alta'
+        }
+    };
+
+    function requestSavedTrends(pluginSlug, data) {
+        // Send a message to the WebView to fetch the trends
+        window.ReactNativeWebView.postMessage(JSON.stringify({
+            messageType: 'FETCH_DATA',
+            name: `${pluginSlug}-trends`,
+            data: data,
+        }));
+    }
+
+    function saveTrends(pluginSlug, data) {
+        // Send a message to the WebView to save the trends
+        window.ReactNativeWebView.postMessage(JSON.stringify({
+            messageType: 'SAVE_DATA',
+            name: `${pluginSlug}-trends`,
+            data: data,
+            time: Date.now(),
+        }));
+    }
+
     try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const trends = [...data[lang].words, ...data[lang].phrases, ...data[lang].hashtags, ...data[lang].globalWords];
+        let trends = [];
+        let savedTrends = [];
+        let timeSavedTrends = 0;
+
+        window.receiveData = function(slug, content) {
+            if (!content) {
+                return;
+            }
+            savedTrends = JSON.parse(content.data);
+            timeSavedTrends = content.time;
+        }
+
+        requestSavedTrends('nightsky-plugin-default', 'trending-topics');
+
+        if(savedTrends.length === 0 || Date.now() - timeSavedTrends > 1000 * 60 * 10) {
+            const response = await fetch(apiUrl).catch((error) => {
+                console.error('Error fetching trends:', error);
+            });
+            console.log('response', response);
+    
+            response.json().then((data) => {
+                trends = [...data[lang].words, ...data[lang].phrases, ...data[lang].hashtags, ...data[lang].globalWords];
+                saveTrends('nightsky-plugin-default', trends);
+            });
+        }
+
+        if (trends.length === 0 && savedTrends.length === 0) {
+            return;
+        }
         
         let displayedTrends = 6;
         const totalTrends = trends.length;
@@ -21,9 +104,10 @@
             trendName.className = 'trend-name';
             trendPosts.className = 'trend-posts';
 
-            trendRank.textContent = `${index + 1} - ${trend.category}`;
+            const categoryTranslation = translations.categories[trend.category][lang];
+            trendRank.textContent = `${index + 1} - ${categoryTranslation}`;
             trendName.textContent = trend.topic.startsWith('#') ? trend.topic : `#${trend.topic}`;
-            trendPosts.textContent = `${(trend.count / 1000).toFixed(1)}mil Posts`;
+            trendPosts.textContent = `${(trend.count / 1000).toFixed(1)}${translations.posts[lang]}`;
 
             trendElement.appendChild(trendRank);
             trendElement.appendChild(trendName);
@@ -42,12 +126,12 @@
             for (let i = 0; i < displayedTrends && i < totalTrends; i++) {
                 ul.appendChild(createTrendElement(trends[i], i));
             }
-            trendingTopicsDiv.innerHTML = '<h2>Trending Topics</h2>';
+            trendingTopicsDiv.innerHTML = `<h2>${translations.trendingTopics[lang]}</h2>`;
             trendingTopicsDiv.appendChild(ul);
 
             if (displayedTrends < totalTrends) {
                 const showMoreButton = document.createElement('button');
-                showMoreButton.textContent = 'Mostrar mais';
+                showMoreButton.textContent = translations.showMore[lang];
                 showMoreButton.addEventListener('click', () => {
                     displayedTrends += 6;
                     displayTrends();
@@ -60,4 +144,45 @@
     } catch (error) {
         console.error('Error fetching trends:', error);
     }
-})();
+};
+
+function isRootUrl() {
+    return window.location.pathname === '/search';
+}
+
+// Observe URL changes and reapply if necessary
+function onUrlChange(callback) {
+    let oldHref = document.location.href;
+
+    const body = document.querySelector("body");
+    const observer = new MutationObserver((mutations) => {
+        if (oldHref !== document.location.href) {
+            oldHref = document.location.href;
+            callback();
+        }
+    });
+
+    observer.observe(body, { childList: true, subtree: true });
+
+    window.addEventListener('popstate', () => {
+        callback();
+    });
+}
+
+// Grants that the script will apply changes correctly after the page is fully loaded or when the DOM changes
+function initTrendingTopics() {
+    if (isRootUrl()) {
+        if (document.readyState === 'complete') {
+            getTrends();
+        } else {
+            document.addEventListener('DOMContentLoaded', getTrends);
+            window.addEventListener('load', getTrends);
+        }
+    }
+}
+
+// Initialize on page load
+initTrendingTopics();
+
+// Reinitialize on URL change
+onUrlChange(initTrendingTopics);
