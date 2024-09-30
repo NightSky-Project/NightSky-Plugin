1a632f035bb462d214a40897852d8311cc7f5c80
Rafael-BD <rafael8885326@gmail.com>
2024-09-30 19:03:38 -0300

    feat: implement logic to manage trending topics removal and addition based on user div count

--- scripts/createTrendingTopic.js
+++ scripts/createTrendingTopic.js
@@ -1,4 +1,5 @@
 console.log('Trending topics script loaded');   
+var removedFeeds = false;
 function addTrendingTopics() {
     const trendingTopicsAlreadyAdded = document.querySelector('.trending-topics');
     if (trendingTopicsAlreadyAdded) {
@@ -20,31 +21,40 @@ function addTrendingTopics() {
     console.log('Adding trending topics');
     // Keep all divs up to the div that contains the button and remove the subsequent ones
     let keep = true;
-    Array.from(suggestedUsersDiv.children).forEach((child) => {
-        if (!keep) { // Remove all subsequent divs
-            suggestedUsersDiv.removeChild(child);
-            return;
-        }
+    console.log(suggestedUsersDiv.children.length);
+    if(!removedFeeds && suggestedUsersDiv.children.length > 5) {
+        Array.from(suggestedUsersDiv.children).forEach((child) => {
+            if (!keep) { // Remove all subsequent divs
+                suggestedUsersDiv.removeChild(child);
+                return;
+            }
 
-        // Check if there is a single div with a single button inside
-        const button = child.childElementCount === 1 && child.children[0].childElementCount === 1 && child.children[0].children[0].tagName === 'BUTTON';
+            // Check if there is a single div with a single button inside
+            const button = child.childElementCount === 1 && child.children[0].childElementCount === 1 && child.children[0].children[0].tagName === 'BUTTON';
 
-        if (button) {
-            keep = false;
-        }
-    });
-
-    if(!document.querySelector('.trending-topics')) {
-        const trendingDiv = document.createElement('div');
-        trendingDiv.classList.add('trending-topics', 'css-175oi2r');
-        suggestedUsersDiv.insertBefore(trendingDiv, suggestedUsersDiv.firstChild);
+            if (button) {
+                keep = false;
+            }
+        });
+        removedFeeds = true;
+    } else {
+        setTimeout(() => {
+            addTrendingTopics();
+        }, 1000);
+        return;
     }
 
     function callGetTrends() {
+        if(!removedFeeds)return;
         try {
             if(!window.getTrends) {
                 setTimeout(callGetTrends, 500);
                 return;
+            }
+            if(!document.querySelector('.trending-topics')) {
+                const trendingDiv = document.createElement('div');
+                trendingDiv.classList.add('trending-topics', 'css-175oi2r');
+                suggestedUsersDiv.insertBefore(trendingDiv, suggestedUsersDiv.firstChild);
             }
             window.getTrends();
         } catch (error) {
@@ -71,13 +81,10 @@ function onUrlChange() {
 
     const observer = new MutationObserver(() => {
         if (isSearchUrl()) {
-            const readyState = document.readyState;
-            if (readyState === 'complete') {
-                if (!document.querySelector('.trending-topics') && !called) {
-                    console.log('Adding trending topics on complete');
-                    window.addTrendingTopics();
-                    called = true;
-                }
+            if (!document.querySelector('.trending-topics') && !called) {
+                console.log('Adding trending topics on URL change');
+                window.addTrendingTopics();
+                called = true;
             }
         } else {
             const trendingTopics = document.querySelector('.trending-topics');
@@ -89,8 +96,11 @@ function onUrlChange() {
     });
 
     observer.observe(body, { childList: true, subtree: true });
+
     window.addEventListener('popstate', () => {
         called = false;
+        observer.disconnect();
+        onUrlChange();
     });
 }
 
